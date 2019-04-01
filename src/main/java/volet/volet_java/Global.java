b33909fdbbe72d8b41b86af9fdc7c4b2e5b1cdd2
linxsus <xavier.gouraud@wanadoo.fr>
2019-04-01 18:04:50 +0200

    Update Global.java

--- src/main/java/volet/volet_java/Global.java
+++ src/main/java/volet/volet_java/Global.java
@@ -2,7 +2,7 @@ package volet.volet_java;
 
 public class Global {
 	public static int NB_MAX_VALEUR=12;
-	public static final String portName= "COM4";// attention
+	public static final String portName= "COM4";
 	public static final int portSpeed= 9600;
 
 }
